5febf41f3c211742cce438d6aeb30dfb5553ce9e
GoldenCredit <birongliu2003@gmail.com>
2023-05-25 19:46:47 -0400

    fix(webhook) resolve webhook duplication issues.

--- src/lib/utils/webhook/WebhookManager.ts
+++ src/lib/utils/webhook/WebhookManager.ts
@@ -1,6 +1,6 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
-import { Types, Webhook, AutoPoster, Embed, Utils } from "../../"
+import {  Webhook, AutoPoster, Embed, Utils, Types } from "../../"
 import { container } from "tsyringe"
 
 export class WebhookManager {
@@ -14,36 +14,36 @@ export class WebhookManager {
 
    public async init() {
       if(!this.manager["ready"]) throw new Error("AutoPoster has not be initalized.");
-      const webhooks = await this.rest.get(Routes.channelWebhooks(this.manager.options.channelId)) as Webhook[];
-      for(const webhook of webhooks) this.store.set(webhook.id, new Webhook(webhook))
-      if(!this.webhook) this.webhook = [...this.store.values()][0]
+      const webhook = await this.fetch();
+      if(!webhook.size) return await this.create()
+      return this.webhook = [...webhook.values()][0]
    }
  
-   public async get(id: string | undefined = this.manager.options.webhookId) {
-      if(this.webhook) return this.webhook;
-      let webhook = this.store.get(id as string) ?? [...this.store.values()][0];
-      if(!webhook) webhook = await this.create(this.manager.options.channelId);
-      return this.webhook = webhook;
+   public async fetch() {
+      const webhooks = await this.rest.get(Routes.channelWebhooks(this.manager.options.channelId)) as Webhook[];
+      return webhooks.reduce((webhooks, webhook) => webhooks.set(webhook.id, webhook), this.store);
    }
 
    public async send(content: Embed[] | Embed | string) {
-      const webhook = await this.get();
+      if(!this.webhook) throw new Error("AutoPoster#init is not called...")
       const body = content instanceof Embed ? { embeds: Utils.toArray(content) } : { content }
-      return this.rest.post(Routes.webhook(webhook.id, webhook.token), {
+      return this.rest.post(Routes.webhook(this.webhook.id, this.webhook.token), {
          body, reason: "Send auto poster messages",
       })
+
    }
 
-   public async create(channelId: string) {
-      const options = await this.rest.post(Routes.channelWebhooks(channelId), {
-         body: { name: this.manager.options.name ?? "AutoPoster" },
-         reason: "Generate webhook for automatically sent ssr contents",
-      }) as Types.WebhookOptions;
-
-      const webhook = new Webhook(options);
-      this.store.set(webhook.id, webhook);
-      if(!this.webhook) this.webhook = webhook;
-      return webhook;
+   public async create() {
+      const data = await this.rest.post(Routes.channelWebhooks(this.manager.options.channelId), {
+         body: {
+            name: "AutoPoster",
+         },
+         reason: "create webhook for auto posting"
+      }) as Types.WebhookOptions
+      const webhook = new Webhook(data);
+      this.store.set(webhook.id, webhook)
+      this.webhook = webhook;
+      return this.webhook;
    }
 
 }
